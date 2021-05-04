a8e872e4e306ab1b9abf48e4b336882086bdb428
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-05-04 16:37:11 +0800

    fill in the operation:{add, subtract, multiply, divide, exponential}

--- 261_a3Yun/src/ComplexNumber.java
+++ 261_a3Yun/src/ComplexNumber.java
@@ -1,95 +1,98 @@
 
-public class ComplexNumber
-{
+public class ComplexNumber {
     /**
      * The real, Re(z), part of the <code>ComplexNumber</code>.
      */
     private double real;
+
     /**
      * The imaginary, Im(z), part of the <code>ComplexNumber</code>.
      */
     private double imaginary;
 
     /**
-     * Constructs a new <code>ComplexNumber</code> object with both real and imaginary parts 0 (z = 0 + 0i).
+     * Constructs a new <code>ComplexNumber</code> object with both real and imaginary parts 0
+     * (z = 0 + 0i).
      */
-    public ComplexNumber()
-    {
+    public ComplexNumber() {
         real = 0.0;
         imaginary = 0.0;
     }
 
     /**
      * Constructs a new <code>ComplexNumber</code> object.
-     * @param real the real part, Re(z), of the complex number
-     * @param imaginary the imaginary part, Im(z), of the complex number
+     * 
+     * @param real
+     *            the real part, Re(z), of the complex number
+     * @param imaginary
+     *            the imaginary part, Im(z), of the complex number
      */
-    public ComplexNumber(double real, double imaginary)
-    {
+    public ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
 
     /**
      * Sets the value of current complex number to the passed complex number.
-     * @param z the complex number
+     * 
+     * @param z
+     *            the complex number
      */
-    public void set(ComplexNumber z)
-    {
+    public void set(ComplexNumber z) {
         this.real = z.real;
         this.imaginary = z.imaginary;
     }
 
     /**
      * The real part of <code>ComplexNumber</code>
+     * 
      * @return the real part of the complex number
      */
-    public double getRe()
-    {
+    public double getRe() {
         return this.real;
     }
 
     /**
      * The imaginary part of <code>ComplexNumber</code>
+     * 
      * @return the imaginary part of the complex number
      */
-    public double getIm()
-    {
+    public double getIm() {
         return this.imaginary;
     }
 
     /**
      * The modulus, magnitude or the absolute value of current complex number.
+     * 
      * @return the magnitude or modulus of current complex number
      */
-    public double mod()
-    {
-        return Math.sqrt(Math.pow(this.real,2) + Math.pow(this.imaginary,2));
+    public double mod() {
+        return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
     }
 
     /**
      * @return the complex number in x + yi format
      */
     @Override
-    public String toString()
-    {
-        String re = this.real+"";
+    public String toString() {
+        String re = this.real + "";
         String im = "";
-        if(this.imaginary < 0)
-            im = this.imaginary+"i";
+        if (this.imaginary < 0)
+            im = this.imaginary + "i";
         else
-            im = "+"+this.imaginary+"i";
-        return re+im;
+            im = "+" + this.imaginary + "i";
+        return re + im;
     }
 
     /**
      * Checks if the passed <code>ComplexNumber</code> is equal to the current.
-     * @param z the complex number to be checked
+     * 
+     * @param z
+     *            the complex number to be checked
      * @return true if they are equal, false otherwise
      */
     @Override
-    public final boolean equals(Object z)
-    {
+    public final boolean equals(Object z) {
         if (!(z instanceof ComplexNumber))
             return false;
         ComplexNumber a = (ComplexNumber) z;
@@ -98,4 +101,79 @@ public class ComplexNumber
 
     // TODO
     // Fill in the operations between complex numbers used for DFT/IDFT/FFT/IFFT.
+
+    /**
+     * Description: <br/>
+     * Add 2 complex numbers together in order to return a new complex number.
+     * 
+     * @author Yun Zhou
+     * @param toAddNumber
+     *            the complex number to be added
+     * @return
+     */
+    public final ComplexNumber add(ComplexNumber toAddNumber) {
+        ComplexNumber currentNumber = this;// the object who calling this method is
+                                           // currentNumber
+        double n_real = currentNumber.getRe() + toAddNumber.getRe();
+        double n_img = currentNumber.getIm() + toAddNumber.getIm();
+        return new ComplexNumber(n_real, n_img);
+    }
+
+    public final ComplexNumber subtract(ComplexNumber toSubtractComplexNumber) {
+        ComplexNumber currentNumber = this;// the object who calling this method is
+                                           // currentNumber
+        double n_real = currentNumber.getRe() - toSubtractComplexNumber.getRe();
+        double n_img = currentNumber.getIm() - toSubtractComplexNumber.getIm();
+        return new ComplexNumber(n_real, n_img);
+    }
+
+    public final ComplexNumber multiply(ComplexNumber toMultiplyComplexNumber) {
+        ComplexNumber currentNumber = this;// the object who calling this method is
+                                           // currentNumber
+
+        double a = currentNumber.getRe();
+        double b = currentNumber.getIm();
+        double c = toMultiplyComplexNumber.getRe();
+        double d = toMultiplyComplexNumber.getIm();
+
+        double n_real = a * c - b * d;
+        double n_img = b * c + a * d;
+        return new ComplexNumber(n_real, n_img);
+    }
+
+    public final ComplexNumber divide(ComplexNumber toDivideComplexNumber) {
+        ComplexNumber currentNumber = this;// the object who calling this method is
+                                           // currentNumber
+
+        double a = currentNumber.getRe();
+        double b = currentNumber.getIm();
+        double c = toDivideComplexNumber.getRe();
+        double d = toDivideComplexNumber.getIm();
+
+        double n_real = (a * c + b * d) / (c * c + d * d);
+        double n_img = (b * c - a * d) / (c * c + d * d);
+        return new ComplexNumber(n_real, n_img);
+    }
+
+    /**
+     * Description: <br/>
+     * Return the new exponential <code>complexNumber</code> of the current
+     * <code>complexNumer</code> object.
+     * 
+     * @author Yun Zhou
+     * @return the new exponential <code>complexNumber</code> of the current
+     *         <code>complexNumer</code> object.
+     */
+    public final ComplexNumber exp() {
+
+        ComplexNumber currentNumber = this;// the object who calling this method is
+                                           // currentNumber
+
+        double a = currentNumber.getRe();
+        double b = currentNumber.getIm();
+
+        double n_real = Math.exp(a) * Math.cos(b);
+        double n_img = Math.exp(a) * Math.sin(b);
+        return new ComplexNumber(n_real, n_img);
+    }
 }
